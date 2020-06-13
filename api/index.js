9a1e00de57b019500914aea486cfd3446002b8a8
José <veganum@hotmail.com>
2020-06-13 23:46:25 +0200

    Avanzando con la api

--- api/index.js
+++ api/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
+//Variable de entorno
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const Users = mongoose.model("User", new mongoose.Schema({ name: String }));
+
+Users.create({ name: "provando cosas" });
+
 app.get("*", (req, res) => {
-  console.log("Hola mundo,mi primera aplicacion  serverless");
-  res.send({ mensaje: "Chanchito feliz" });
+  Users.find().then((x) => res.send(x));
 });
 
 module.exports = app;
