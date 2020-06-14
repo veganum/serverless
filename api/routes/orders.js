356685e002eca03aa4a87e5fcc48076091f5caf0
José <veganum@hotmail.com>
2020-06-14 21:25:53 +0200

    mejorando rutas

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -2,8 +2,25 @@ const express = require("express");
 
 const router = express.Router();
 
+//Listar
 router.get("/", (req, res) => {
   res.send("hola soy orders");
+});
+//Buscar un elemento en particular
+router.get("/:id", (req, res) => {
+  res.send(req.params.id);
+});
+//Crear
+router.post("/", (req, res) => {
+  res.send("soy post");
+});
+//Remplazar
+router.put("/:id", (req, res) => {
+  res.send("soy put");
+});
+//Borrar
+router.delete("/:id", (req, res) => {
+  res.send("soy delete");
 });
 
 module.exports = router;
--- api/routes/plates.js
+++ api/routes/plates.js
@@ -6,4 +6,20 @@ router.get("/", (req, res) => {
   res.send("hola soy plates");
 });
 
+router.get("/:id", (req, res) => {
+  res.send(req.params.id);
+});
+//Crear
+router.post("/", (req, res) => {
+  res.send("soy post");
+});
+//Remplazar
+router.put("/:id", (req, res) => {
+  res.send("soy put");
+});
+//Borrar
+router.delete("/:id", (req, res) => {
+  res.send("soy delete");
+});
+
 module.exports = router;
